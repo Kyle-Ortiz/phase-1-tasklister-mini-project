1a3d89cee10d385efd397195a7e199c00a20e1c3
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-10 16:48:53 -0500

    todo list version 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userInput = document.getElementById("new-task-description").value;
+    const newTask = document.createElement("li");
+    newTask.innerText = userInput;
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(newTask);
+  });
 });
